43658e0261ee912cb3746f7e52b517b3d9642bea
Rhys Tumelty <rhys@tumelty.co.uk>
2024-11-16 21:58:30 +0000

    Simple canvas app

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,27 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final clearPaint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+    final rectPaint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), clearPaint);
+
+    canvas.drawRect(
+        Rect.fromLTWH(2 * (size.width / 100), 2 * (size.width / 100),
+            0.5 * (size.width / 10), 0.5 * (size.width / 10)),
+        rectPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -46,11 +67,23 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-          child: Text(
-        '$_counter',
-        style: Theme.of(context).textTheme.headlineMedium,
-      )),
+      body: Stack(
+        children: [
+          Expanded(
+            child: Container(
+                color: Colors.black,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: CustomPaint(
+                      painter: MyPainter(),
+                    ),
+                  ),
+                )),
+          ),
+        ],
+      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
